34be42d1f36b496f83b8900aff30f97a4f8b26ba
Nabeel Tariq <nabeeltariq2@gmail.com>
2018-03-05 11:52:54 -0500

    'surprise'

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ import os
 from sqlalchemy import *
 
 
-DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:sesame@localhost/amazonmusicalins3?charset=utf8')
+# DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:sesame@localhost/amazonmusicalins3?charset=utf8')
+DB_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://nabeeltariq2:nabeeltariq2@aa1byyhqu3w2u2t.cpliemudyxqt.us-east-1.rds.amazonaws.com/ebdb?charset=utf8')
